a849b47f52e968b2cffb7d998cebc8662105a7b9
Zhaofeng Zhou <zhaofengzhou9@gmail.com>
2022-10-26 10:33:41 +0800

    maven bug检查脚本完成

--- maven-bug.sh
+++ maven-bug.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
+# 获取打包的版本信息
+version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
 # 移除无用的文件
-rm -rf /c/Users/zhouzhaofeng/Documents/maven/apache-maven/target/
+rm -rf apache-maven-*
 
 # maven 打包
 mvn clean package -Dmaven.test.skip=true -Drat.skip=true
 
-# 获取打包的版本信息
-version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-
 # 解压打包好的 zip 包
-OUTPUT_DIRECTOR=/c/Users/zhouzhaofeng/Documents/maven/apache-maven/target 
-unzip $OUTPUT_DIRECTOR/apache-maven-$version-bin.zip
+unzip apache-maven/target/apache-maven-$version-bin.zip -d .
 
 # 使用打包的 maven 编译项目
-$OUTPUT_DIRECTOR/apache-maven-$version/bin/mvn -f /c/Users/zhouzhaofeng/Documents/maven-issue-reproduction/pom.xml compile
+apache-maven-$version/bin/mvn -f /c/Users/zhouzhaofeng/Documents/maven-issue-reproduction/pom.xml compile
 
 
